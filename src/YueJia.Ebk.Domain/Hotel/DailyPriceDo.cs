09c217eadcb57c658c9231828fa9773184552021
caxin <11360847@qq.com>
2025-06-04 17:02:41 +0800

    增加 每日库存、每日价格表

---
+++ src/YueJia.Ebk.Domain/Hotel/DailyPriceDo.cs
@@ -0,0 +1,33 @@
+﻿namespace YueJia.Ebk.Domain.Hotel;
+
+/// <summary>
+/// 每日价格
+/// </summary>
+[SugarTable("DailyPrice", "每日价格")]
+public record DailyPriceDo : EntityTenant
+{
+
+    /// <summary>
+    /// 房间Id
+    /// </summary>
+    [SugarColumn(ColumnDescription = "房间Id")]
+    public long RoomId { get; init; }
+
+    /// <summary>
+    /// 价格计划Id
+    /// </summary>
+    [SugarColumn(ColumnDescription = "价格计划Id")]
+    public long PricePlanId { get; init; }
+
+    /// <summary>
+    /// 当前日期
+    /// </summary>
+    [SugarColumn(ColumnDescription = "当前日期", ColumnDataType = "date")]
+    public DateTime CurrentDate { get; private set; }
+
+    /// <summary>
+    /// 价格
+    /// </summary>
+    [SugarColumn(ColumnDescription = "价格", ColumnDataType = "decimal(8,2)")]
+    public decimal Price { get; private set; }
+}
