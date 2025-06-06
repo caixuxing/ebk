942406b812d4ca4a0957247078892678e6cf6c4e
tc <11360847@qq.com>
2025-06-06 17:42:52 +0800

    1

--- src/YueJia.Ebk.Web/Controllers/HotelController.cs
+++ src/YueJia.Ebk.Web/Controllers/HotelController.cs
@@ -476,6 +476,27 @@ public class HotelController : AbpController
     }
     #endregion
 
+    #region 批量编辑库存与价格
 
+    /// <summary>
+    /// 批量编辑库存与价格
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IActionResult> BatchEditInventoryAndPricesMgr(string userHotelId)
+    {
+        //加载酒店信息
+        ViewBag.hotelModel = await HotelPublishApp.GetHotelPublishDetailAsync(Convert.ToInt64(userHotelId));
+        ViewBag.mv = new ViewHotelVo()
+        {
+            Id = userHotelId,
+            HotelName = $@"{(ViewBag.hotelModel as HotelPublishDetailDto).HotelNameEn} [{(ViewBag.hotelModel as HotelPublishDetailDto).HotelName}]"
+        };
+        ViewBag.userRoomList = await HotelApp.GetHotelRoomListByIdAsync(userHotelId.ToLong());
+        return View();
+    }
+
+
+
+    #endregion
 
 }
